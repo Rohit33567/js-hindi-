80eeede84776c1f4e05e4918c83feec7c01c7753
Rohit33567 <rohitsuryavanshi225@gmail.com>
2025-09-09 14:43:26 +0530

    Add files via upload

---
+++ 01_objects.js
@@ -0,0 +1,34 @@
+// const tinderUers = new Object()
+const tinderUers = {}
+
+tinderUers.id =" 1324avd"
+tinderUers.name = " roht"
+tinderUers.isloggedIn = false
+
+// console.log(tinderUers);
+
+const regularUser = {
+        email:  "@rohit123.com",
+        Fullname: {
+           userfullname: {
+            firstname: "rohit",
+            lastname:  "suryavnshi" 
+        }
+          
+    }
+       
+
+}
+// console.log(regularUser.Fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b", 3: "c"}
+const obj2 = {a: "1", b: "3", c: "8"}
+console.log(obj1,obj2);
+
+       
+        
+    
+         
+
+console.log(regularUser.Fullname);
+
